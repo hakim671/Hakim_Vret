1f819477b9382fcfdbbeddb08ed9ca00860cf148
hakim671 <boyzodahakim2008@gmail.com>
2024-10-15 20:07:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 year = st.number_input("Возраст",value=10)
-name = input("Ваше имя")
 ves = st.number_input("Вес",value=30, step=10)
 rost = st.number_input("Вес",value=150, step=10)
 if st.button("Начать прогноз"):
