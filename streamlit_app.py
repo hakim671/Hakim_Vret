4e23fb0c206eec4741cf9d0e53d0b2e3fac29762
hakim671 <boyzodahakim2008@gmail.com>
2024-10-15 20:11:10 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,5 @@ import streamlit as st
 year = st.number_input("Возраст",value=10)
 ves = st.number_input("Вес",value=30, step=10)
 rost = st.number_input("Рост",value=150, step=10)
-name = input("Имя")
 if st.button("Начать прогноз"):
   st.write("Zoir Гей")
