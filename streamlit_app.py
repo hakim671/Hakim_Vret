3d30b054b01854936a6fd7a9968c89e40afb6506
hakim671 <boyzodahakim2008@gmail.com>
2024-11-09 21:06:29 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 
-# Проверка загрузки файла
+# Проверка на загрузку файла
 try:
     df = pd.read_excel('hoby.xlsx')
     st.write("Файл успешно загружен.")
 except FileNotFoundError:
-    # Если файла нет, создаем пустой DataFrame с нужными колонками
+    # Если файла нет, создаем пустой DataFrame
     df = pd.DataFrame(columns=['full_name', 'Hoby'])
     st.write("Файл не найден. Создан новый DataFrame.")
 
@@ -22,8 +22,11 @@ if name and hobby:
         df = pd.concat([df, df2], ignore_index=True)
         # Проверка содержимого перед записью
         st.write("DataFrame для записи:", df)
+        
         # Попытка записи в файл
         try:
+            # Очистка кэша перед записью
+            st.cache_data.clear()
             df.to_excel('hoby.xlsx', index=False)
             st.write('Вы записаны в базу')
         except Exception as e:
