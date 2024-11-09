6920598949dc156093ba48ae8498b2c489fe21f3
hakim671 <boyzodahakim2008@gmail.com>
2024-11-09 20:38:04 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import pandas as pd
 df = pd.read_excel('hoby.xlsx')
 name = st.text_input('Полное Имя')
 hobby = st.text_input('Хобби')
-df2 = pd.DataFrame({'full_name':name, 'Hoby':hobby})
+df2 = pd.DataFrame({'full_name':[name], 'Hoby':[hobby]})
 if(st.button('Внести в базу')):
-  df = pd.concat(df,df2)
-  df.to_excel('hoby.xlsx')
+  df = pd.concat([df,df2], ignore_index=True)
+  df.to_excel('hoby.xlsx',index=False)
   st.write('Вы записаны в базу')
